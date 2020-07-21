390b00ca73f752a35d17a3fe2d39a7b481007a50
John <jabatzoglou@ucmerced.edu>
2020-07-21 13:06:51 -0700

    Update crossbarrier_new.m

--- crossbarrier_new.m
+++ crossbarrier_new.m
@@ -16,19 +16,21 @@ tlim=find(m.lat>=tmin & m.lat<=tmax);
 alim=find(m.lon>=amin & m.lon<=amax);
 
 % load in topographic aspect
-wdir4=m.wdir4(tlim,alim);
-wdir32=m.wdir8(tlim,alim);
+tasp4=m.terrainaspect4(tlim,alim);
+tasp32=m.terrainaspect32(tlim,alim);
 
-% wdir4 is the aspect of topography at a 4-km grid
-% wdir32 is aspect at 32-km grid
+% tasp4 is the aspect of topography at a 4-km grid
+% tasp32 is aspect at 32-km grid
 
 [alon,alat]=meshgrid(m.lon(1,alim),m.lat(tlim,1));
 [lon,lat]=meshgrid(lon,lat);
+
 %do it at 4km scale and 32km scale
 WDIR=winddir(u,v);
 ws=reshape(sqrt(u(:).^2+v(:).^2),size(u));
 clear u v
 
+% bilinearly interpolate weather outputs to terrain grid
 if ndims(WDIR)==2
     WDIR4=int16(interp2(lon,lat,double(WDIR),alon,alat));
     ws4=(interp2(lon,lat,double(ws),alon,alat));
@@ -41,11 +43,13 @@ end
 
 ws4=single(ws4);
 
-difwind4=abs(single(WDIR4-wdir4))/180*pi;
-difwind32=abs(single(WDIR4-wdir32))/180*pi;
+% this is just the difference in radians between local aspect and wind direction
+difwind4=(single(WDIR4-tasp4))/180*pi;
+difwind32=(single(WDIR4-tasp32))/180*pi;
 
-crossbarrierwind32=abs(sin(difwind32)).*ws4;
-crossbarrierwind4=abs(sin(difwind4)).*ws4;
+% crossbarrier wind is simply the wind speed multiplied by the directional component parallel to aspect
+crossbarrierwind32=abs(cos(difwind32)).*ws4;
+crossbarrierwind4=abs(cos(difwind4)).*ws4;
 
 % this looks for the maximum cross barrier wind using a 4x4 moving window ~60km;
 % this can be adjusted for domain size
